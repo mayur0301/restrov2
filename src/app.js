2c79546460369a6954c3ea40c9b9b34ca548c2e0
Mayur <rajsalve2022@gmail.com>
2026-01-22 18:02:24 +0530

    New Frntend URL

--- src/app.js
+++ src/app.js
@@ -5,7 +5,7 @@ const cors = require('cors')
 
 app.use(cors({
     origin: ["http://localhost:5173",
-        "https://chilli-chhat.vercel.app/"
+        "https://chilli-chhat.vercel.app"
     ],
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
