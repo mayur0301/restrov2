917c30f8a7f95d0ff34c1f19d58f256af2643fdf
Mayur <rajsalve2022@gmail.com>
2026-01-24 16:22:34 +0530

    Updated Cookie Added & Logout

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -75,10 +75,11 @@ exports.login = async (req, res) => {
   // 🍪 SET COOKIE
   res.cookie('token', token, {
     httpOnly: true,
-    secure: false,     // true in production (HTTPS)
-    sameSite: 'lax',
-    maxAge: 7 * 24 * 60 * 60 * 1000 // 7 days
+    secure: true,        // 🔥 MUST be true in prod
+    sameSite: 'none',    // 🔥 MUST be 'none' for cross-site
+    maxAge: 7 * 24 * 60 * 60 * 1000
   })
+
 
   res.json({
     message: 'Login successful',
@@ -96,8 +97,8 @@ exports.login = async (req, res) => {
 exports.logout = async (req, res) => {
   res.clearCookie('token', {
     httpOnly: true,
-    sameSite: 'lax',
-    secure: false // true in prod
+    sameSite: 'none',
+    secure: true // true in prod
   })
 
   res.json({
